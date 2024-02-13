1b85ce4e5f9cb323e8390dfad8049cac58a8b31e
Luis Paredes <lparedesr@fi.uba.ar>
2024-02-13 12:46:29 -0300

    wip: connected all_news_dataset with the endpoint

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+import pandas
 
 app = FastAPI()
+news_dataset = pandas.read_csv("all_the_news.csv")
 
 @app.get("/tfidf")
-def calculate_tfidf(url : str, limit : int = 1):
+def calculate_tfidf(url : str, limit : int = 10):
     return {
     "terms": [
             {
                 "term": "term1",
                 "tf-idf": 2.5
+            },
+            {
+                "term": news_dataset["id"].unique().size,
+                "tf-idf": 1
             }
         ]
     }
