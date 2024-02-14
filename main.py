cc518693c36e64df19d97e7b192f4e8b05e5b5b0
Luis Paredes <lparedesr@fi.uba.ar>
2024-02-14 18:55:50 -0300

    doc: added documentation for better readability

--- main.py
+++ main.py
@@ -17,21 +17,27 @@ def calculate_tfidf(url : str, limit : int = 10):
     if (scraped_text is None):
         raise HTTPException(status_code=404, detail="URL not found")
 
-    documents = news_dataset["title_and_content"].to_list()
-    documents.append(scraped_text)
+    # Get corpus for the TF-IDF
+    corpus = news_dataset["title_and_content"].to_list()
+    corpus.append(scraped_text)
 
     tfidf = TfidfVectorizer(stop_words="english", lowercase=True, strip_accents='unicode')
 
-    # get tf-df values
-    result = tfidf.fit_transform(documents)
+    # get tf-df values for every document
+    result = tfidf.fit_transform(corpus)
 
-    tfidf_ordered = sorted(zip(tfidf.get_feature_names_out(), result[-1].toarray()[0]), key=lambda x: x[1], reverse=True)
+    # get the TF-IDF values on the url page.
+    tfidf_url_page = result[-1].toarray()[0]
+
+    # Order the page's TF-IDF in descending order
+    # We save it in (term, tf-idf) tuple
+    ordered_tfidf_url_page = sorted(zip(tfidf.get_feature_names_out(), tfidf_url_page), key=lambda x: x[1], reverse=True)
     
     return {
         "terms": [
             {
                 "term": term,
                 "tf-idf": tfidf
-            } for term, tfidf in tfidf_ordered[:limit]
+            } for term, tfidf in ordered_tfidf_url_page[:limit]
         ]
     }
