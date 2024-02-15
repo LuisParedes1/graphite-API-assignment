aab67fe9782fe98943654a2473527edb63e61db2
Luis Paredes <lparedesr@fi.uba.ar>
2024-02-15 12:20:15 -0300

    refactor: now the API uses the trained model instead of training it each time

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 from fastapi import FastAPI, HTTPException
-import pandas
 
 from scrapper import scrape_url
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
 
 
 app = FastAPI()
-news_dataset = pandas.read_csv("all_the_news.csv")
 
+# Loading the trained model
+tfidf_model = pickle.load(open("tfidf_vectorizer_model.joblib", 'rb'))
 
 @app.get("/tfidf")
 def calculate_tfidf(url : str, limit : int = 10):
 
-    scraped_text : str | None = scrape_url(url)
+    scraped_document : str | None = scrape_url(url)
     
-    if (scraped_text is None):
+    if (scraped_document is None):
         raise HTTPException(status_code=404, detail="URL not found")
+    
 
-    # Get corpus for the TF-IDF
-    corpus = news_dataset["title_and_content"].to_list()
-    corpus.append(scraped_text)
+    tfidf_url_page = tfidf_model.transform([scraped_document]).toarray()[0]
 
-    tfidf = TfidfVectorizer(stop_words="english", lowercase=True, strip_accents='unicode')
-
-    # get tf-df values for every document
-    result = tfidf.fit_transform(corpus)
-
-    # get the TF-IDF values on the url page.
-    tfidf_url_page = result[-1].toarray()[0]
-
-    # Order the page's TF-IDF in descending order
-    # We save it in (term, tf-idf) tuple
-    ordered_tfidf_url_page = sorted(zip(tfidf.get_feature_names_out(), tfidf_url_page), key=lambda x: x[1], reverse=True)
+    ordered_tfidf_url_page = sorted(zip(tfidf_model.get_feature_names_out(),
+                                    tfidf_url_page), key=lambda x: x[1], reverse=True )
     
     return {
         "terms": [
